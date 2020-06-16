c02bcd81d420233b8db12daf168353a7660f0030
medamer <edamer.mo@gmail.com>
2020-06-15 17:48:44 -0700

    insert the data insto db

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,25 +1,33 @@
 # web_app/routes/book_routes.py
 
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from web_app.models import Book, db, parse_records
 
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    # books = [
+    #     {"id": 1, "title": "Book 1"},
+    #     {"id": 2, "title": "Book 2"},
+    #     {"id": 3, "title": "Book 3"},
+    # ]
+    # SELECT * FROM books
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records)
     return jsonify(books)
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    # books = [
+    #     {"id": 1, "title": "Book 1"},
+    #     {"id": 2, "title": "Book 2"},
+    #     {"id": 3, "title": "Book 3"},
+    # ]
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records)
     return render_template("books.html", message="Here's some books", books=books)
 
 @book_routes.route("/books/new")
@@ -29,9 +37,15 @@ def new_book():
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
+
+    # INSERT INTO books ...
+    new_book = Book(title=request.form["book_title"], author_id=request.form["author_name"])
+    db.session.add(new_book)
+    db.session.commit()
+
     # todo: store in database
     return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
+        "message": "BOOK CREATED OK",
         "book": dict(request.form)
     })
     #flash(f"Book '{new_book.title}' created successfully!", "success")
